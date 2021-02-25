089ac33be05885847507f553deea3a2ed05b1a1f
Isaiah Murray <murray.isaiah13@gmail.com>
2021-02-25 16:09:28 -0500

    added .slice method

--- weekThree/arrays/slice.js
+++ weekThree/arrays/slice.js
@@ -1,17 +1,44 @@
 //! The .slice method will return a copy of specified portion of an array.
 //! The portion is specified by the index given in the parameters
 
-const animals = ['ant', 'bison', 'camel', 'duck', 'elephant'];
+const foods = [
+    { name: "bread", price: 100 },
+    { name: "beef", price: 200 },
+    { name: "mint", price: 10 },
+    { name: "shrimp", price: 5 },
+    { name: "ketchup", price: 500 },
+    { name: "bell pepper", price: 25 },
+  ];
 
 
+console.log(foods.slice(2, 4));
+/* expected output: 
+[
+    { name: "mint", price: 10}, 
+    { name: "shrimp", price: 5 }, 
+    { name: "ketchup", price: 500 }
+]
+*/
 
-console.log(animals.slice(2, 4));
-// expected output: Array ["camel", "duck"]
 
-console.log(animals.slice(1, 5));
-// expected output: Array ["bison", "camel", "duck", "elephant"]
-
+console.log(foods.slice(1, 5));
+/* expected output:
+[
+    { name: "beef", price: 200 },
+    { name: "mint", price: 10 },
+    { name: "shrimp", price: 5 },
+    { name: "ketchup", price: 500 },
+    { name: "bell pepper", price: 25 }
+]
+*/
 //?If the second parameter is not specified, the method will the rest of the 
 //?array after the first specified index
-console.log(animals.slice(2));
-// expected output: Array ["camel", "duck", "elephant"]
+console.log(foods.slice(2));
+/* expected output: 
+[
+    { name: "mint", price: 10 },
+    { name: "shrimp", price: 5 },
+    { name: "ketchup", price: 500 },
+    { name: "bell pepper", price: 25 },
+]
+*/
