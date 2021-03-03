82738c617493bea5546576d9fe846dd35840289d
Isaiah Murray <murray.isaiah13@gmail.com>
2021-03-03 13:50:33 -0500

    Reorganizing Notes

---
+++ Basics/Objects/objectLiteral.js
@@ -0,0 +1,24 @@
+
+//Objects use curly bois
+let person = {
+    firstName: "isaiah",
+    lastName: "murray",
+    age: 23,
+    height: 6.3,
+    hobbies: ['coding', 'gaming'],
+    favoriteFoods:{
+        fruits: ['apple', 'orange', 'banana']
+    }
+}
+
+//arrays us square bois
+
+let fruits = ['apple', 'orange', 'banana'];
+
+for(let index = 1; index <= 5; index++){
+    console.log(index);
+}
+
+for(let index = 5; index >= 0; index--){
+    console.log(index);
+}
